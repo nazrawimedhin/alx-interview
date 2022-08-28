3a73fe4936ec5826292dd755228b8943b2fbb4bf
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-08-28 15:58:36 +0300

    pascal's triangle: trial 5

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Pascal Triangle Interview Challenge"""
 
+
 def pascal_triangle(n):
     """returns a list of lists of numbers
     representing the pascal triangle"""
@@ -11,13 +12,13 @@ def pascal_triangle(n):
     pascal_triangle = [0] * n
 
     for i in range(n):
-        # define a row and fill first and last idx with 1
+        # define a row and fill first and last index with 1
         new_row = [0] * (i+1)
         new_row[0] = 1
         new_row[len(new_row) - 1] = 1
 
         for j in range(1, i):
-            if j > 0 and j < len(new_row):
+            if j >= 1 and j < len(new_row) - 1:
                 a = pascal_triangle[i - 1][j]
                 b = pascal_triangle[i - 1][j - 1]
                 new_row[j] = a + b
